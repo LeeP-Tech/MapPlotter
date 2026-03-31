93ebaa2363a435e58a70c815239f97a9d95985cd
Lee Pasifull <lee@pasifull.co.uk>
2026-03-31 17:38:14 +0100

    fix: update routing and base path in configuration files

--- src/router.tsx
+++ src/router.tsx
@@ -3,13 +3,6 @@ import Layout from "@/pages/_layout"
 import HomePage from "@/pages/home"
 import NotFoundPage from "@/pages/not-found"
 
-// IMPORTANT: Do not remove or modify the code below!
-// Normalize basename when hosted in Power Apps
-const BASENAME = new URL(".", location.href).pathname
-if (location.pathname.endsWith("/index.html")) {
-  history.replaceState(null, "", BASENAME + location.search + location.hash);
-}
-
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -19,6 +12,6 @@ export const router = createBrowserRouter([
       { index: true, element: <HomePage /> },
     ],
   },
-], { 
-  basename: BASENAME // IMPORTANT: Set basename for proper routing when hosted in Power Apps
+], {
+  basename: '/mapplotter',
 })
